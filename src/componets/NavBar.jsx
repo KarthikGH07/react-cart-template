c765842e8af2aaa85bcbacd1fcc7adfa1961d6a9
KarthikGH07 <karthikhegde96@gmail.com>
2021-03-24 16:28:27 +0530

    Initial Commit

---
+++ src/componets/NavBar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
